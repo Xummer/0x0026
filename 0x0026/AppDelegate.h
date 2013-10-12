c31a0e084b8074ec47f7d0dbea2bdb067cc42f83
Xummer <xummer26@gmail.com>
2013-10-12 16:30:32 +0800

    Initial Commit

---
+++ 0x0026/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  0x0026
+//
+//  Created by Xummer on 13-10-12.
+//  Copyright (c) 2013年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 0x0026Tests/_x0026Tests.m
@@ -0,0 +1,34 @@
+//
+//  _x0026Tests.m
+//  0x0026Tests
+//
+//  Created by Xummer on 13-10-12.
+//  Copyright (c) 2013年 Xummer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _x0026Tests : XCTestCase
+
+@end
+
+@implementation _x0026Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
